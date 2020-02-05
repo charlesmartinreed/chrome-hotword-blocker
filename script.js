39b111084bc479bcaee37d4ab5fdf5818976169d
Charles Martin Reed <charlesmartinreed@pop-os.localdomain>
2020-02-04 21:54:22 -0600

    Update replaceText to black out mentions of word.

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function replaceText(element) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
         // use textContent instead of innerText here becauase we're dealing with a text node, not an element, per se
-        element.textContent = element.textContent.replace(/coronavirus/gi, 'REDACTED')
+        if (element.textContent.match(/coronavirus/gi)) {
+            // this blacks out the entire paragraph
+            // element.parentElement.style.color = 'black';
+            // element.parentElement.style.backgroundColor = 'black';
+
+            // essentially, we create a new element that captures the redacted text by using interpolation and then use that element to replace the old element
+            const newEl = document.createElement('span');
+            newEl.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span style="background-color: black; color: black;">$1</span>')
+            element.replaceWith(newEl)
+        }
+        // element.textContent = element.textContent.replace(/coronavirus/gi, 'REDACTED')
     }
 }
