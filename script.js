74d613e0415b1016e3b405f16543aee6fbedfb74
Charles Martin Reed <charlesmartinreed@pop-os.localdomain>
2020-02-04 22:28:59 -0600

    Add rainbow color effect to text.

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 // replaceText is a recursive function that calls itself until its conditional logic is no longer truthy
 
-replaceText(document.body)
+replaceText(document.body, "coronavirus")
 
-function replaceText(element) {
+function replaceText(element, phrase) {
+    // let regex = new RegExp(phrase, "gi")
     if (element.hasChildNodes()) {
         // loop until we get through all the text based child elements
-        element.childNodes.forEach(replaceText)
+        element.childNodes.forEach(replaceText, phrase)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        // use textContent instead of innerText here becauase we're dealing with a text node, not an element, per se
         if (element.textContent.match(/coronavirus/gi)) {
-            // this blacks out the entire paragraph
-            // element.parentElement.style.color = 'black';
-            // element.parentElement.style.backgroundColor = 'black';
-
-            // essentially, we create a new element that captures the redacted text by using interpolation and then use that element to replace the old element
-            const newEl = document.createElement('span');
-            newEl.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span style="background-color: black; color: black;">$1</span>')
-            element.replaceWith(newEl)
+        newEl = document.createElement('span');
+        newEl.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>')
+        element.replaceWith(newEl)
         }
-        // element.textContent = element.textContent.replace(/coronavirus/gi, 'REDACTED')
     }
 }
